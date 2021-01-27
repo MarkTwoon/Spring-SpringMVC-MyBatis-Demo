e6535b2f228d1da0de9ce78169e60531f3922468
MarkTwoon <154593566@qq.com>
2021-01-27 10:33:40 +0800

    Mr.Yi  MyBatis框架 2021.1.27 10:33 第三次提交

---
+++ MyBatisDemo/src/com/chinasoft/MyBatisDemo/config/MyBatisConfig.java
@@ -0,0 +1,36 @@
+package com.chinasoft.MyBatisDemo.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisConfig {
+public static  SqlSession openMyBatis(){
+	SqlSessionFactory   sqlSessionFactory;
+	SqlSession session=null;
+	/*注意打开mybatis链接时  读取的是mybatis的  配置管理xml文件*/
+	String resource="mybatis.cfg.xml";
+	InputStream  inputStream;
+	
+	try {
+		inputStream=Resources.getResourceAsStream(resource);
+		sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+		session=sqlSessionFactory.openSession();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return  session;
+}
+	
+public static void closeMyBatis(SqlSession session){
+	if(session!=null){
+		session.close();
+	}
+}
+	
+}
---
+++ MyBatisDemo/src/com/chinasoft/MyBatisDemo/dao/IAdminDao.java
@@ -0,0 +1,8 @@
+package com.chinasoft.MyBatisDemo.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IAdminDao {
+public List<Map<String,Object>>  selectGoodsAll();
+}
