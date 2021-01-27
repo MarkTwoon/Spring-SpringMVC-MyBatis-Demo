3cd49fd3902270cc513d05669191c18cb3130d48
MarkTwoon <154593566@qq.com>
2021-01-27 14:43:42 +0800

    Mr.Yi  2021.1.27  14:43  MyBatis框架添加二级缓存

--- MyBatisDemo/src/com/chinasoft/MyBatisDemo/servlet/DemoServlet.java
+++ MyBatisDemo/src/com/chinasoft/MyBatisDemo/servlet/DemoServlet.java
@@ -20,12 +20,18 @@ public class DemoServlet {
     /*dao层 抽象实例化*/
     IAdminDao mapper=session.getMapper(IAdminDao.class);
     /*调用dao层 执行对应的SQL方法*/
-    List<Map<String,Object>>  list=mapper.selectGoodsAll();
+   /* List<Map<String,Object>>  list=mapper.selectGoodsAll();
     System.out.println(list);
     System.out.println("SQL指令执行成功");
     
-    List<Map<String,Object>> userList=mapper.selectUserAll();
-    System.out.println(userList);
+    List<Map<String,Object>> userList=mapper.selectGoodsAll();
+    System.out.println(userList);*/
+    for(int i=0;i<50;i++){
+    	List<Map<String,Object>>  list=mapper.selectGoodsAll();
+    	System.out.println(list);
+        System.out.println("SQL指令执行成功，第"+i+"次");
+    }
+    
     
     MyBatisConfig.closeMyBatis(session); 
 		 
